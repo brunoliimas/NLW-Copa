1cc053c8b72c6a318e5152b7d63a37814dd10284
Bruno Lima <ibrunoliimas@gmail.com>
2022-11-09 00:58:12 -0300

    Terceira Aula

--- mobile/App.tsx
+++ mobile/App.tsx
@@ -2,8 +2,13 @@ import React from 'react';
 import { NativeBaseProvider, StatusBar } from "native-base";
 import { useFonts, Roboto_400Regular, Roboto_500Medium, Roboto_700Bold } from '@expo-google-fonts/roboto'
 
+import { AuthContextProvider } from './src/context/AuthContext';
+
+import { Pools } from './src/screens/Pools';
+// import { Find } from './src/screens/Find';
+// import { New } from './src/screens/New';
+// import { SingIn } from './src/screens/SignIn';
 import { Loading } from './src/components/Loading';
-import { SingIn } from './src/screens/SignIn';
 
 import { THEME } from './src/styles/theme';
 
@@ -11,16 +16,16 @@ import { THEME } from './src/styles/theme';
 export default function App() {
   const [fontsLoaded] = useFonts({ Roboto_400Regular, Roboto_500Medium, Roboto_700Bold })
 
-
-
   return (
     <NativeBaseProvider theme={THEME}>
-      <StatusBar
-        barStyle='light-content'
-        backgroundColor={'transparent'}
-        translucent
-      />
-        {fontsLoaded ? <SingIn /> : <Loading />}
+      <AuthContextProvider>
+        <StatusBar
+          barStyle='light-content'
+          backgroundColor={'transparent'}
+          translucent
+        />
+        {fontsLoaded ? <Pools /> : <Loading />}
+      </AuthContextProvider>
     </NativeBaseProvider>
   );
 }
